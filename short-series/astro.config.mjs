f6b307802354bff09f9e9386da14d5525beabae6
robzzzzu-cmd <robzzzzu@gmail.com>
2026-08-25 22:24:50 +0300

    Update astro.config.mjs

--- short-series/astro.config.mjs
+++ short-series/astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://robzzzzu-cmd.github.io',
   base: '/AI-farm-w-',
-  integrations: [mdx(), sitemap()],
 });
