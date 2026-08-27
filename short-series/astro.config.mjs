42440750453517adf87058053f1798ef709e7208
robzzzzu-cmd <robzzzzu@gmail.com>
2026-08-27 13:35:02 +0300

    Update astro.config.mjs

--- short-series/astro.config.mjs
+++ short-series/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://robzzzzu-cmd.github.io',
-  base: '/AI-farm-w-',
+  site: 'https://tradeopportunities.trade',
+  base: '/',
 });
